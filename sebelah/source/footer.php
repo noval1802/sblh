e0c672c35b51d955609339c32a7ce3bdf97ee342
noval1802 <abdulazizkon@gmail.com>
2022-12-31 02:57:44 +0700

    -

---
+++ sebelah/source/footer.php
@@ -0,0 +1,62 @@
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-info">
+                            <h3> <span> CV.</span> Cipta <span>Mandiri</span> Perkasa</h3>
+                            <p>Jl. Raya Tambelang Kp. Pisang Batu Desa Kertamukti Kec. Cibitung Kab.Bekasi 17520<br><br>
+                                <strong>Phone :</strong> 0813 8622 5702<br>
+                                <strong>Email :</strong> pemudasebelah1511@gmail.com<br>
+                                <strong>Click Here :<i class='bx bx-downvote bx-fade-down' style='color:#fffefe'></i><i
+                                        class='bx bx-downvote bx-fade-down' style='color:#fffefe'></i></strong>
+                            </p>
+                            <div class="social-links mt-3">
+                                <a href="https://wa.me/qr/JFH2GSYU3KESN1" class="twitter"><i
+                                        class="bx bxl-whatsapp"></i></a>
+                                <a href="https://www.facebook.com/ahmad.nursaid.7731" class="facebook"><i
+                                        class="bx bxl-facebook"></i></a>
+                                <a href="https://instagram.com/ahmad.nursaid?igshid=YmMyMTA2M2Y=" class="instagram"><i
+                                        class="bx bxl-instagram"></i></a>
+                                <a href="mailto: midun_sep73@yahoo.com" class="google-plus"><i
+                                        class="bx bx-envelope"></i></a>
+                                <a href="https://goo.gl/maps/hX3UfZi9MBqDVVyn9" class="location"><i
+                                        class="bx bx-current-location"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#about">About</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#services">Services</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="portfolio-details.php">Details Portfolio</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="copyright">
+                    &copy; 2023 <strong>SBLH</strong>
+                </div>
+                <div class="credits">Designed by <a href="https://linktr.ee/noval1802">noval</a>
+                </div>
+            </div>
+    </footer>
+    <!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</html>
